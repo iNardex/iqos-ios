565404ff2149fd8dd9c59bbad94bc48bcdd54b78
willy <william.nardo@welld.ch>
2020-09-24 00:14:01 +0200

    animation

--- iquos/AnimationExtension.swift
+++ iquos/AnimationExtension.swift
@@ -5,4 +5,16 @@
 //  Created by William Nardo on 21/09/2020.
 //
 
+import UIKit
 import Foundation
+
+extension UILabel {
+    func blink() {
+        self.alpha = 0.0;
+        UIView.animate(withDuration: 0.8, //Time duration you want,
+            delay: 0.0,
+            options: [.curveEaseInOut, .autoreverse, .repeat],
+            animations: { [weak self] in self?.alpha = 1.0 },
+            completion: { [weak self] _ in self?.alpha = 0.0 })
+    }
+}
